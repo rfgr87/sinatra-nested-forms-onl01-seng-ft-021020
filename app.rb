75236eec69588179cc69651548b9157292f91c90
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-03-24 23:55:10 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,6 +4,20 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
-
+  
+  get '/new' do
+    erb :new
   end
+  
+  post '/pirates' do
+    @pirate = Pirate.new(params[:pirate])
+
+    params[:pirate][:ships].each do |details|
+      Ship.new(details)
+    end
+    @ships = Ship.all
+
+    erb :'pirates/show'
+  end
+end
 end
